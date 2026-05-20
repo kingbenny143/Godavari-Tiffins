6fb0f0408bab6a80685e3f376d73ac879b62267e
Anish Raj Anand <anandanishraj@gmail.com>
2026-05-20 17:33:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+   
+function toggleGallery(){
+document.getElementById("gallerySection").classList.toggle("hidden");
+}
+
+function openPreview(img){
+    const modal = document.getElementById("previewModal");
+    const preview = document.getElementById("previewImg");
+
+    preview.src = img.src;
+
+    modal.classList.remove("hidden");
+    modal.style.opacity = "0";
+
+    setTimeout(() => {
+        modal.style.opacity = "1";
+    }, 10);
+}
+
+function closePreview(){
+document.getElementById("previewModal").classList.add("hidden");
+}
+
+const gallery = document.getElementById("galleryScroll");
+const dots = document.getElementById("dots");
+
+if(gallery){
+const imgs = gallery.querySelectorAll("img");
+imgs.forEach((_,i)=>{
+let d=document.createElement("span");
+if(i==0)d.classList.add("active");
+dots.appendChild(d);
+});
+
+gallery.addEventListener("scroll",()=>{
+let i=Math.round(gallery.scrollLeft/210);
+dots.querySelectorAll("span").forEach(d=>d.classList.remove("active"));
+if(dots.children[i]) dots.children[i].classList.add("active");
+});
+}
+
+function setLang(lang){
+document.querySelectorAll('.en').forEach(e=>e.style.display=(lang==='en')?'block':'none');
+document.querySelectorAll('.te').forEach(e=>e.style.display=(lang==='te')?'block':'none');
+}
+
+function setLang(lang){
+document.querySelectorAll('.en').forEach(e=>e.style.display=(lang==='en')?'block':'none');
+document.querySelectorAll('.te').forEach(e=>e.style.display=(lang==='te')?'block':'none');
+}
